0d71c79a6d78a3cec63e45d3e4c2a9b9063adf7b
yangzhiling <229050938@qq.com>
2019-10-16 15:03:51 +0800

    添加

---
+++ mygit/mygit/ff.h
@@ -0,0 +1,17 @@
+//
+//  ff.h
+//  mygit
+//
+//  Created by yangzhiling on 2019/10/16.
+//  Copyright © 2019 yangzhiling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ff : SZYJsonModel
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ mygit/mygit/ff.m
@@ -0,0 +1,13 @@
+//
+//  ff.m
+//  mygit
+//
+//  Created by yangzhiling on 2019/10/16.
+//  Copyright © 2019 yangzhiling. All rights reserved.
+//
+
+#import "ff.h"
+
+@implementation ff
+
+@end
